0e53fbcf006e74cbe934960efe10a17a1f0d449c
zhenfuyuu <asears1092@conestogac.on.ca>
2025-03-12 22:23:24 -0400

    Update focas5 c file

--- Focused-Assignments/FOCAS5_ArraysNPointers/pointer_array_manipulation.c
+++ Focused-Assignments/FOCAS5_ArraysNPointers/pointer_array_manipulation.c
@@ -22,6 +22,18 @@ const int array[SIZE] = {10, 0, 345, -567, 7,
                          89, 256, 512, -2048, 4096};
 int *arrayPtr = array; //Declaring pointer to array (points to first element by default)
 
-
+return 0;
+}
+/*
+FUNCTION: void getSum
+DESCRIPTION: Calculates the sum of the array elements.
+PARAMETERS:
+int *arrayPtr: Pointer parameter for the array
+size_t arraySize: Gets the size of the array and points to it
+RETURNS:
+Calculated sum of the array.
+*/
+void getSum (int *arrayPtr, size_t arraySize)
+{
 
 }
