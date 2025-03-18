4dea9a4216ca7785229cb8bbde91342b57484b17
zhenfuyuu <asears1092@conestogac.on.ca>
2025-03-17 22:46:49 -0400

    update focas5.c

--- Focused-Assignments/FOCAS5_ArraysNPointers/pointer_array_manipulation.c
+++ Focused-Assignments/FOCAS5_ArraysNPointers/pointer_array_manipulation.c
@@ -12,7 +12,7 @@ It will also calculate the sum and average of the elements in the array using po
 
 //Function prototypes
 void getSum (const int *arrayPtr, size_t arraySize, int *sumPtr);
-void getAvg(int *arrayPtr, size_t arraySize);
+void getAvg(const int *arrayPtr, size_t arraySize, double *avgPtr);
 
 int main (void)
 {
@@ -27,6 +27,9 @@ int *sumPtr = &sum; //pointing to sum value to insert into function
 getSum(arrayPtr, SIZE, sumPtr);
 printf("The sum of the array elements is %d\n", sum);
 //outout average
+double average = 0; //initalize average
+double *avgPtr = &average; //pointer to average variable
+getAvg(arrayPtr, SIZE, avgPtr);
 
 return 0;
 }
@@ -35,8 +38,8 @@ FUNCTION: void getSum
 DESCRIPTION: Calculates the sum of the array elements.
 PARAMETERS:
 const int *arrayPtr: Pointer parameter for the array
-size_t arraySize: Gets the size of the array and points to it
-int *sumPtr: pointer parameter in order to return the sum
+size_t arraySize: Gets the SIZE of the array and points to it
+int *sumPtr: Pointer parameter in order to return the sum
 RETURNS:
 Calculated sum of the array.
 */
@@ -44,8 +47,30 @@ void getSum (const int *arrayPtr, size_t arraySize, int *sumPtr)
 {
 for (size_t i = 0; i < arraySize; i++) //looping through array
 {
-*sumPtr = *sumPtr + *arrayPtr;
+*sumPtr = *sumPtr + *arrayPtr; //defines the pointer to add the current sum + the array elements
 arrayPtr++;
 //returns sum of the array elements
 }
+}
+/*
+FUNCTION: getAvg
+DESCRIPTION:
+Calculates the average of the array's index elements
+PARAMETERS:
+const int *arrayPtr: Pointer parameter to the array
+size_t arraySize: Gets the SIZE of the array and points to it
+double *avgPtr: Pointer parameter in order to return the averag
+RETURNS:
+Calculated average of the array
+*/
+void getAvg(const int *arrayPtr, size_t arraySize, double *avgPtr)
+{
+//Adding the sum before division
+for (size_t i = 0; i < arraySize; i++)
+{
+*(int*)avgPtr = *(int*)avgPtr + *arrayPtr; //Casting avgPtr to int to add total sum before division
+arrayPtr++;
+}
+//Divide average by total number of index's in array
+*avgPtr = *avgPtr / (double)arraySize;
 }
